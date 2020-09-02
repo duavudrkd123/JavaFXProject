0935f05f94a1e0765c6d4b271a86b58f7d0a635a
Kim Sang Min <yhoo112@naver.com>
2020-09-02 15:11:41 +0900

    0902class

--- JavaFXProject/src/basic/control/InputController.java
+++ JavaFXProject/src/basic/control/InputController.java
@@ -1,6 +1,10 @@
 package basic.control;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -56,8 +60,45 @@ public class InputController implements Initializable {
 //			showPopup("날짜를 선택하세요!!");
 		} else if (dateExit.getValue() == null) {
 			showCustomDialog("날짜를 입력하세요");
+		} else {
+			insetData();
 		}
 	}
+	
+	public void insetData() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "hr", passwd="hr";
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, passwd);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		String sql = "insert into new_board values(?,?,?,?,?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setNString(1,	txtTitle.getText());
+			pstmt.setNString(2,	txtPassword.getText());
+			pstmt.setNString(3, comboPublic.getValue());
+			pstmt.setNString(4, dateExit.getValue().toString());
+			pstmt.setNString(5, txtContent.getText());
+			
+			int r = pstmt.executeUpdate();
+			System.out.println(r +"건 입력됨.");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	
 	public void showCustomDialog(String msg) {
 		Stage stage = new Stage(StageStyle.UTILITY); //닫기만 있는 형식
@@ -116,5 +157,7 @@ public class InputController implements Initializable {
 		pop.setAutoHide(true);
 		pop.show(btnReg.getScene().getWindow());
 	}
+	
+	
 
 } // end of Initializable
